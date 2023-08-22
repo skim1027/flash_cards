01d4a32397405d7d044797b1745b0c1b7fca3e49
Sooyung Kim <kimx1279@gmail.com>
2023-08-22 15:23:42 -0600

    Update spec and class for deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,17 +1,31 @@
 class Deck
-  attr_reader :cards, :count, :cards_in_category
+  attr_reader :cards, :count, :cards_in_category, :category
   def initialize(cards)
-    
     @cards = cards
-    
+
   end
 
   def count
-    @count = cards.count 
+    cards.count 
   end
 
-  def cards_in_category
-    @cards_in_category == @category
+  def cards_in_category(cat)
+    new_card = []
+    @cards.each do |card|
+      if card.category == cat
+        new_card << card
+      end
+    end
+    new_card
   end
+
+
+  # def cards_in_category(string)
+  #   card_category = @cards.select do |card|
+  #     card.category == string
+  #   end
+  # end
+
+
 end
 
