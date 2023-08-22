f9ebd786a150e964e4649906dc9f27dbe309ff71
Sooyung Kim <kimx1279@gmail.com>
2023-08-21 20:06:53 -0600

    Add test for Deck

---
+++ spec/deck_spec.rb
@@ -0,0 +1,37 @@
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  it 'stores cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    expect(cards).to eq([card_1, card_2, card_3])
+  end
+
+  xit 'creates a deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    expect(deck.cards).to be(cards)
+  end
+
+  xit 'counts how many cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    expect(deck.count).to eq(3)
+  end
+
+  xit 'display category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    expect(deck.cards_in_category).to eq(card.category)
+  end
+
+end
